70e78c88482561f9353f669b5502c1b9c79ab510
Nick Janetakis <nick.janetakis@gmail.com>
2021-02-22 11:10:14 -0500

    Fix indentation for CELERY_CONFIG

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379/0")
 
 # Celery.
 CELERY_CONFIG = {
-  "broker_url": REDIS_URL,
-  "result_backend": REDIS_URL,
-  "include": []
+    "broker_url": REDIS_URL,
+    "result_backend": REDIS_URL,
+    "include": []
 }
