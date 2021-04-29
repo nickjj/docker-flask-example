6f9de8836bedbf168a2b2218b299abd0fdf7860e
Nick Janetakis <nick.janetakis@gmail.com>
2021-04-29 17:34:14 -0400

    Remove unused REDIS_URL import

--- hello/extensions.py
+++ hello/extensions.py
@@ -2,8 +2,6 @@ from flask_debugtoolbar import DebugToolbarExtension
 from flask_sqlalchemy import SQLAlchemy
 from flask_static_digest import FlaskStaticDigest
 
-from config.settings import REDIS_URL
-
 
 debug_toolbar = DebugToolbarExtension()
 db = SQLAlchemy()
