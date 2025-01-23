96d7068499999e14aa4b035ea9ceb757658f3cb2
Nick Janetakis <nick.janetakis@gmail.com>
2025-01-23 08:59:33 -0500

    Update to Tailwind v4

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,7 +1,3 @@
-module.exports = {
-  content: [
-    '/app/assets/js/**/*.js',
-    '/app/assets/css/**/*.css',
-    '/app/hello/**/*.html'
-  ],
-}
+export default {
+  plugins: ["@tailwindcss/postcss"],
+};
